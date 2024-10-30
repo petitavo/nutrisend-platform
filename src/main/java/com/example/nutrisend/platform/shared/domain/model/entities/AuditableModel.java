509acb9fd2eb23fedc6be64d8a3231f7b1681a32
C7leo <149843920+C7leo@users.noreply.github.com>
2024-10-30 15:44:07 -0500

    feat(shared): added files

---
+++ src/main/java/com/example/nutrisend/platform/shared/domain/model/entities/AuditableModel.java
@@ -0,0 +1,28 @@
+package com.example.nutrisend.platform.shared.domain.model.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class AuditableModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    private Long id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    @Getter
+    private Date createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    @Getter
+    private Date updatedAt;
+}
