1f367e4d3bb75eab186603a808a9e40eb8bdfe85
C7leo <149843920+C7leo@users.noreply.github.com>
2024-10-30 15:13:18 -0500

    feat(nutrisend-platform): initial commit

---
+++ src/main/java/com/example/nutrisend/platform/NutrisendPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.nutrisend.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrisendPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrisendPlatformApplication.class, args);
+	}
+
+}
