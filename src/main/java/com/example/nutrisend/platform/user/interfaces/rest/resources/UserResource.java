7bf2cc030ce528e2cc1dc741fd3733fe2af43314
LuisVJ97 <u201717523@upc.edu.pe>
2024-10-30 22:03:13 -0500

    feat(user): Create and added resources and transform

---
+++ src/main/java/com/example/nutrisend/platform/user/interfaces/rest/resources/UserResource.java
@@ -0,0 +1,5 @@
+package com.example.nutrisend.platform.user.interfaces.rest.resources;
+
+public record UserResource(Long id,String name, String surname, String email) {
+
+}
---
+++ src/main/java/com/example/nutrisend/platform/user/interfaces/rest/transform/CreateUserCommandFromResourceAssembler.java
@@ -0,0 +1,4 @@
+package com.example.nutrisend.platform.user.interfaces.rest.transform;
+
+public class CreateUserCommandFromResourceAssembler {
+}
