a79402351eeb9f5fc8c23484d80e737455d07bd9
Luis Alfonso Villegas Jipa <u201717523@upc.edu.pe>
2024-11-13 16:35:06 -0500

    feat(order): Update reateOrderItemCommand

--- src/main/java/com/example/nutrisend/platform/order/domain/model/commands/CreateOrderItemCommand.java
+++ src/main/java/com/example/nutrisend/platform/order/domain/model/commands/CreateOrderItemCommand.java
@@ -1,17 +1,13 @@
 package com.example.nutrisend.platform.order.domain.model.commands;
 
 public record CreateOrderItemCommand(
-        String name,
-        double price,
-        String category,
+        int id,
         int quantity
 ) {
     public CreateOrderItemCommand {
-        if (name == null || name.isBlank()) {
-            throw new IllegalArgumentException("Name cannot be null or empty");
-        }
         if (quantity <= 0) {
             throw new IllegalArgumentException("Quantity must be greater than zero");
         }
     }
 }
+
