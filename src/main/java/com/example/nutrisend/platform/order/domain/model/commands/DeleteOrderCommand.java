d207aa375fb9eb63492ca5e5d7ee1de35ee88447
Luis Alfonso Villegas Jipa <u201717523@upc.edu.pe>
2024-11-13 16:37:01 -0500

    feat(order): Update DeleteOrderCommand

--- src/main/java/com/example/nutrisend/platform/order/domain/model/commands/DeleteOrderCommand.java
+++ src/main/java/com/example/nutrisend/platform/order/domain/model/commands/DeleteOrderCommand.java
@@ -1,4 +1,9 @@
 package com.example.nutrisend.platform.order.domain.model.commands;
 
 public record DeleteOrderCommand(Long orderId) {
+    public DeleteOrderCommand {
+        if (orderId == null) {
+            throw new IllegalArgumentException("Order ID cannot be null");
+        }
+    }
 }
