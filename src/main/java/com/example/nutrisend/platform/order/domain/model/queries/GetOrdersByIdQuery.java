9faecfd7558d0729cb46db85a8cd852ebe92c02e
Luis Alfonso Villegas Jipa <u201717523@upc.edu.pe>
2024-11-13 16:39:30 -0500

    feat(order): Update GetOrdersByIdQuery for Long Id

--- src/main/java/com/example/nutrisend/platform/order/domain/model/queries/GetOrdersByIdQuery.java
+++ src/main/java/com/example/nutrisend/platform/order/domain/model/queries/GetOrdersByIdQuery.java
@@ -1,8 +1,8 @@
 package com.example.nutrisend.platform.order.domain.model.queries;
 
-public record GetOrdersByIdQuery(String id) {
+public record GetOrdersByIdQuery(Long id) {
     public GetOrdersByIdQuery {
-        if (id == null || id.isBlank()) {
+        if (id == null) {
             throw new IllegalArgumentException("ID cannot be null or empty");
         }
     }
