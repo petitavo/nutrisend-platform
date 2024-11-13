5dc4d38e63726a4cbcdc5e9abe10dfdf64fc12a9
Luis Alfonso Villegas Jipa <u201717523@upc.edu.pe>
2024-11-13 16:38:04 -0500

    feat(order): Update DeleteOrderCommand

--- src/main/java/com/example/nutrisend/platform/order/domain/model/commands/UpdateOrderCommand.java
+++ src/main/java/com/example/nutrisend/platform/order/domain/model/commands/UpdateOrderCommand.java
@@ -1,5 +1,17 @@
 package com.example.nutrisend.platform.order.domain.model.commands;
 
+import java.util.List;
 
-public record UpdateOrderCommand() {
+public record UpdateOrderCommand(
+        Long orderId,
+        List<UpdateOrderItemCommand> items // Lista de ítems a actualizar
+) {
+    public UpdateOrderCommand {
+        if (orderId == null) {
+            throw new IllegalArgumentException("Order ID cannot be null");
+        }
+        if (items == null || items.isEmpty()) {
+            throw new IllegalArgumentException("Items cannot be null or empty");
+        }
+    }
 }
