b57c1b1e07d71a69fd15e46cc0cc796328249ec1
Luis Alfonso Villegas Jipa <u201717523@upc.edu.pe>
2024-11-13 16:42:01 -0500

    feat(order): Update OrderRepository with Long

--- src/main/java/com/example/nutrisend/platform/order/infrastructure/persistence/jpa/repositories/OrderRepository.java
+++ src/main/java/com/example/nutrisend/platform/order/infrastructure/persistence/jpa/repositories/OrderRepository.java
@@ -1,10 +1,10 @@
-package com.example.nutrisend.platform.order.jpa;
+package com.example.nutrisend.platform.order.infrastructure.persistence.jpa.repositories;
 
 import com.example.nutrisend.platform.order.domain.model.aggregates.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, String> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
 }
