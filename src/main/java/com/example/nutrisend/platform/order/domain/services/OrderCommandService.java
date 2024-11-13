54894c2554047a64f2a945b0760434f4627aefe6
Luis Alfonso Villegas Jipa <u201717523@upc.edu.pe>
2024-11-13 16:40:36 -0500

    feat(order): Update OrderCommandService with update and delete command

--- src/main/java/com/example/nutrisend/platform/order/domain/services/OrderCommandService.java
+++ src/main/java/com/example/nutrisend/platform/order/domain/services/OrderCommandService.java
@@ -2,9 +2,15 @@ package com.example.nutrisend.platform.order.domain.services;
 
 import com.example.nutrisend.platform.order.domain.model.aggregates.Order;
 import com.example.nutrisend.platform.order.domain.model.commands.CreateOrderCommand;
+import com.example.nutrisend.platform.order.domain.model.commands.DeleteOrderCommand;
+import com.example.nutrisend.platform.order.domain.model.commands.UpdateOrderCommand;
 
 import java.util.Optional;
 
 public interface OrderCommandService {
     Optional<Order> handle(CreateOrderCommand command);
+
+    Optional<Order> update(UpdateOrderCommand command);
+
+    boolean delete(DeleteOrderCommand command);
 }
